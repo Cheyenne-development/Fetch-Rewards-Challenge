5a5bc73c2d435cca88491b927df9e1954422594d
Cheyenne McMahon <orlandu8@gmail.com>
2021-11-04 14:58:11 -0500

    Star with empty store.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,3 @@
-const points = [
-  { payer: "DANNON", points: 3000, timestamp: "2020-10-31T10:00:00Z" },
-  { payer: "UNILEVER", points: 200, timestamp: "2020-10-31T11:00:00Z" },
-  { payer: "DANNON", points: 2100, timestamp: "2020-10-31T15:00:00Z" },
-  { payer: "MILLER COORS", points: 10000, timestamp: "2020-11-01T14:00:00Z" },
-  { payer: "DANNON", points: 1000, timestamp: "2020-11-02T14:00:00Z" },
-];
+const points = [];
 
-module.exports = { points };
+module.exports = { points }
